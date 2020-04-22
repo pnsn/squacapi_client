1aff49f0a763e9d04373c1ea5aaf90a721f978e6
Jon Connolly <jontconnolly@gmail.com>
2020-04-22 15:00:44 -0700

    add mv and sed command to generate script

--- generate_swagger.sh
+++ generate_swagger.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+# Usage: to update, checkout a branch then run. 
 docker run -v ${PWD}:/local/ --rm swaggerapi/swagger-codegen-cli \
 	generate \
 		-i https://squacapi.pnsn.org/swagger.json \
 		-l python \
 		-c /local/config.json \
 		-o /local 
+mv ./squacapi/api/_api.py ./squacapi/api/default_api.py
+sed -i '' 's/http:\/\/squacapi/https:\/\/squacapi/g' ./squacapi/configuration.py
