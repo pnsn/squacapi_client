cb42f19bb6e377948b7cbf4d6615587ffbcb5a77
Kyla Marczewski <marczk@uw.edu>
2022-08-23 13:19:20 -0700

    remove old text

--- generate_swagger.sh
+++ generate_swagger.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 # Usage:
 #   checkout a branch then run. 
-# 	TOKEN=supersecret ./generate_swagger.sh
+# 	./generate_swagger.sh
 # To get token go to https://squacapi.pnsn.org/v1.0/user/token/
-# to see a list of generate args
-#TOKEN=supersecrettoken docker run -v ${PWD}:/tmp --rm swaggerapi/swagger-codegen-cli help generate
+
 echo "Select squacapi host: "
 HOST=""
 
@@ -40,7 +39,4 @@ docker run -it --platform linux/amd64 \
 		-o /local \
 		-a "Authorization: Token $TOKEN"
 		
-
-
-#mv ./squacapi_client/api/_api.py ./squacapi_client/api/default_api.py
 sed -i '' 's/http:\/\/squacapi/https:\/\/squacapi/g' ./squacapi_client/configuration.py
