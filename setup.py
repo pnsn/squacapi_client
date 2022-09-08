96eae29710eab8d179e40fe354422e3d162521fb
Kyla Marczewski <marczk@uw.edu>
2022-09-08 14:44:59 -0700

    readd utilities

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "squacapi_client"
-VERSION = "1.0.0"
+VERSION = "2.1.0"
 # To install the library, run the following
 #
 # python setup.py install
